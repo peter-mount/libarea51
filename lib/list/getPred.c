a031393f9333e217c7dfdc1208e44d250538cd27
peter <peter@europa>
2016-05-13 18:08:45 +0100

    Convert to a shared library

--- lib/list/getPred.c
+++ lib/list/getPred.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "area51/list.h"
 
-struct Node *list_getNext(struct Node *n) {
+struct Node *list_getPred(struct Node *n) {
     if (list_isNode(n) && list_isNode(n->n_pred))
         return n->n_pred;
     else
