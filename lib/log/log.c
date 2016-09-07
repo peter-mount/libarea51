234c3a2291d677bb11f2af0c7d1eeab1d1d5b68e
peter <peter@europa>
2016-09-07 17:57:47 +0100

    Ensure logging is thread safe

--- lib/log/log.c
+++ lib/log/log.c
@@ -4,11 +4,22 @@
 #include <stdarg.h>
 #include <time.h>
 #include <area51/log.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+static bool init = false;
+static pthread_mutex_t mutex;
 
 void logconsole(const char *fmt, ...) {
+    if (!init) {
+        pthread_mutex_init(&mutex, NULL);
+        init = true;
+    } else
+        pthread_mutex_lock(&mutex);
+
     time_t now = time(NULL);
     struct tm tm;
-    localtime_r(&now,&tm);
+    localtime_r(&now, &tm);
     char buf[20];
     strftime(buf, 20, "%Y-%m-%d %H:%M:%S", &tm);
 
@@ -20,5 +31,7 @@ void logconsole(const char *fmt, ...) {
     vfprintf(stdout, fmt, argp);
     fputc(10, stdout);
     va_end(argp);
+
+    pthread_mutex_unlock(&mutex);
 }
 
